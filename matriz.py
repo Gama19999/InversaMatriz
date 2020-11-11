03dfdd4661784eba6e87b00301affb4911085f90
AlberoArfax <alberoarfax7175@gmail.com>
2020-11-11 12:37:09 -0600

    Agregando metodos de llenado de matriz

--- matriz.py
+++ matriz.py
@@ -2,7 +2,7 @@ class Matriz:
     def __init__(self, filas, columnas, valor=0):
         self.filas = filas
         self.columnas = columnas
-        self.matriz = [[]]
+        self.matriz = []
         for fila in range(filas):
             for _ in range(columnas):
                 self.matriz[fila].append(valor)
@@ -13,3 +13,16 @@ class Matriz:
         for fila in range(self.filas):
             cadena += str(self.matriz[fila]) + '\n'
         return cadena
+
+    def rellenarMatrizDados(self):
+        for i in range(self.filas):
+            self.matriz.append([])
+            for j in range(self.columnas):
+                nuevonum = int(input(f"Numero a ingresar en la posicion {i}, {j}: "))
+                self.matriz[i].append(nuevonum)
+
+    def rellenarMatrizEspecifico(self, num):
+        for i in range(self.filas):
+            self.matriz.append([])
+            for j in range(self.columnas):
+                self.matriz[i].append(num) 
