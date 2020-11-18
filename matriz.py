af099cfebf60177bc70435e07b8f6617a63d931e
AlberoArfax <alberoarfax7175@gmail.com>
2020-11-18 13:11:50 -0600

    Creacion y desarrollo de metodo laplace

--- matriz.py
+++ matriz.py
@@ -21,6 +21,11 @@ class Matriz:
             cadena += str(self.matriz[fila]) + '\n'
         return cadena
 
+    def rellenarConListaDeLista(self, lis):
+        for i in range(self.filas):
+            for j in range(self.columnas):
+                self.matriz[i][j] = lis[i][j]
+
     def rellenarMatrizDados(self):
         for i in range(self.filas):
             for j in range(self.columnas):
@@ -44,21 +49,21 @@ class Matriz:
             cadena += "]\n"
         return cadena
 
-    def possigno(self):
-        posf = int(input("Posicion fila ")) - 1
-        posc = int(input("Posicion columna ")) - 1
+    def possigno(self,posf, posc):
+        #posf = int(input("Posicion fila ")) - 1
+        #posc = int(input("Posicion columna ")) - 1
         if ((posf + posc) % 2) == 0:
-            return "+"
+            return 1
         else:
-            return "-"
+            return -1
 
     def obtenerDeterminante(self):
         acumSumas1 = 0
         acumSumas2 = 0
-        if tam == 2:
+        if self.filas == 2:
             determinante = (self.matriz[0][0] * self.matriz[1][1]) - (self.matriz[1][0] * self.matriz[0][1])
             print(f"El determinante de la matriz es {determinante}")
-        elif tam == 3:
+        elif self.filas == 3:
             self.matriz.append(self.matriz[0])
             self.matriz.append(self.matriz[1])
 
@@ -80,6 +85,39 @@ class Matriz:
         else:
             print("Con ese tamaño no podemos hacer este metodo, una disculpa")
 
+    def determinanteLaplace(self):
+        lisCeros = []
+        lisPivotes = []
+        lisObjetos = []
+        acumSumas = 0;
+        for i in range(self.filas):
+            lisCeros.append(0)
+            for j in range(self.columnas):
+                if self.matriz[i][j] == 0:
+                    lisCeros[i] += 1
+
+        filMasCeros = lisCeros.index(max(lisCeros))
+
+        for j in range(self.filas):
+            lisPivotes.append(self.matriz[filMasCeros][j] * self.possigno(filMasCeros-1, j-1))
+
+        for k in range(self.filas):
+            matrizAceptados = []
+            for i in range(self.filas):
+                if i != filMasCeros:
+                    matrizAceptados.append([])
+                    for j in range(self.columnas):
+                        if j != lisPivotes[i]:
+                            matrizAceptados[i].append(self.matriz[i][j])
+            obj = Matriz(self.filas - 1, self.columnas - 1)
+            obj.rellenarConListaDeLista(matrizAceptados)
+            lisObjetos.append(obj)
+
+        for i in range(len(lisObjetos)):
+            acumSumas += lisObjetos[i].obtenerDeterminante() * lisPivotes[i]
+
+        print(f"El determinante de la matriz es {acumSumas}")
+
 
 autores()
 tam = int(input("Tamaño de la matriz: "))
@@ -90,4 +128,4 @@ print(a)
 # print(a.signos())
 # print(a.possigno())
 
-a.obtenerDeterminante()
+a.determinanteLaplace()
